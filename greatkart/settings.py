5c91b071d4c627b5372f5389e4ebd23c9ec61823
Habibur-Rahman0927 <habibur.rahaman.0927@gmail.com>
2022-03-03 01:24:11 +0600

    updated cart icon functionality

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
 
                 'category.context_proccessors.menu_list',
+                'carts.context_processors.counter',
             ],
         },
     },
