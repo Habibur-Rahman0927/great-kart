87520216dec390075dbc948fdb289af31d4aeb03
Habibur-Rahman0927 <habibur.rahaman.0927@gmail.com>
2022-03-03 00:04:27 +0600

    data updated in cart page

--- carts/views.py
+++ carts/views.py
@@ -51,10 +51,12 @@ def cart(request, total=0, quantity=0, cart_item=None):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
+        tax = (2 * total)/100
+        grand_total = total + tax
     except ObjectNotExist:
         pass
 
-    context = {'total': total, 'quantity': quantity, 'cart_items': cart_items}
+    context = {'total': total, 'quantity': quantity, 'cart_items': cart_items, 'tax': tax, 'grand_total': grand_total}
     return render(request, 'store/cart.html', context)
 
 
