916c4a28d61ee936cee65c6f3730462f08a5b94a
Habibur-Rahman0927 <habibur.rahaman.0927@gmail.com>
2022-03-03 23:21:13 +0600

    registration done

--- accounts/form.py
+++ accounts/form.py
@@ -12,6 +12,14 @@ class RegistrationForm(forms.ModelForm):
         model = Account
         fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
 
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        pasword = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if pasword != confirm_password:
+            raise forms.ValidationError('Password does not match!')
+
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
         self.fields['first_name'].widget.attrs['placeholder'] = 'Enter First Name'
@@ -21,3 +29,5 @@ class RegistrationForm(forms.ModelForm):
 
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
+    
+    
