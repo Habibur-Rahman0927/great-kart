45ac259a9b0d794587563c138dc1f71d9f7acb52
Habibur-Rahman0927 <habibur.rahaman.0927@gmail.com>
2022-03-03 22:44:23 +0600

    edit registration form

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-
+from .form import RegistrationForm
 # Create your views here.
 
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    form = RegistrationForm()
+    context = {'form': form}
+    return render(request, 'accounts/register.html', context)
 
 def login(request):
     return render(request, 'accounts/login.html')
